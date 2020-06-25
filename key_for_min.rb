23b9bf361201de031792d5a4ce864a7e6d8508b3
Jennifer Grenier <jenniferagrenier@gmail.com>
2020-06-25 15:18:17 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,9 +3,9 @@
 #hash = {:blake => 500, :ashley => 2, :adam => 1}
 
 def key_for_min_value(name_hash)
-  value = 1000 
+  value = 1000
   name_hash.each do |name, total|
-    if total < value 
-      value += 1 
+    if total < value
+      value += 1
     end 
-end  
+end
