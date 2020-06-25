05a6f4ac26fd7824893360ed734ac95f7fe29dfc
Jennifer Grenier <jenniferagrenier@gmail.com>
2020-06-25 15:28:36 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,15 +4,15 @@
 
 def key_for_min_value(name_hash)
   lowest_number = nil
-  name_hash.each do |name, number|
-    if lowest_number == nil
-      answer = name
-      lowest_number = number
-    end
-    if lowest_number > number
-      answer = name
-      lowest_number = number
-end
-end
-
+  name_hash.each do |name,number|
+   if lowest_number == nil
+    answer = name
+    lowest_number = number
+   end
+ elsif lowest_number > number
+     answer = name
+     lowest_number = number
+   end
+  end
+  answer
 end
